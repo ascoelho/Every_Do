3c80acfaefa9e135688f6f78cc082b655e7a4ccd
ascoelho <anthonystefancoelho@gmail.com>
2016-05-17 11:26:45 -0400

    first commit

---
+++ Every.Do/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Every.Do
+//
+//  Created by Anthony Coelho on 2016-05-17.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Anthony Coelho on 2016-05-17.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
